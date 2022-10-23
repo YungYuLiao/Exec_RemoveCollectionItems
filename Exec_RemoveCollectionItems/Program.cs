c7a0f0a1797b6f260b9b09df725079329f3f99d3
88698 <88698@DESKTOP-Q0Q2VCJ>
2022-10-23 20:33:52 +0800

    加入專案檔案。

---
+++ Exec_RemoveCollectionItems/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_RemoveCollectionItems
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{ //在集合中移除多個項目
+			List<string> list = new List<string>{"Annie","Bill","Cara","Dorthy","Elle","Flora"};
+			list.RemoveRange(0,1); //移除一個範圍 (0,1)表示從0號位移除1個
+			list.Remove("Cara"); //移除單個項目
+			list.RemoveAt(list.Count-2); //索引處項目
+			//list.RemoveAll(list.Contains); //移除所有,可用list.Clear()
+			foreach (var item in list)
+			{
+				Console.WriteLine(item);
+			}
+			
+
+		}
+	}
+}
---
+++ 計算BMI/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 計算BMI
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Person person =	new Person();
+			string height = person.GetHeight();
+			string weight = person.GetWeight();
+			try
+			{
+				double index = person.BMIcal(double.Parse(height), double.Parse(weight));
+				string result = person.Display(index);
+				Console.WriteLine($"您的BMI為{index.ToString("#.00")}，{result}");
+			}
+			catch (Exception ex)
+			{
+
+				Console.WriteLine(ex.Message);
+			}
+				
+		
+
+		}
+		class Person
+		{
+			public string GetHeight()
+			{
+				Console.Write("請輸入身高(公分): ");
+				return Console.ReadLine();
+			}
+			public string GetWeight()
+			{
+				Console.Write("請輸入體重(公斤): ");
+				return Console.ReadLine();
+			}
+			public double BMIcal(double height, double weight)
+			{
+				if (height <= 0 || weight <= 0)
+				{
+					throw new Exception("輸入數值不可小於或等於0");
+				}
+				double BMI = weight / Math.Pow(height/100, 2);
+				return BMI;
+			}
+			public string  Display (double BMI)
+			{
+				string msg = String.Empty;
+				if (BMI < 18.5)
+				{
+					msg = "體重過輕!";
+				}
+				else if (BMI < 24)
+				{
+					msg = "體重適中";
+				}
+				else if (BMI < 27)
+				{
+					msg = "體重過重!";
+				}
+				else if (BMI < 30)
+				{
+					msg = "輕度肥胖!";
+				}
+				else if (BMI < 35)
+				{
+					msg = "中度肥胖!";
+				}
+				else
+				{
+					msg = "重度肥胖!";
+				}
+				return msg;
+			}
+		}
+	}
+}
